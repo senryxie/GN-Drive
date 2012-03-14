9cd46c089f11ba14ae24fc4c96d150011ac3017c
ofshellohicy <ofshellohicy@gmail.com>
2012-03-14 12:48:31 +0800

    fix sqlstore class

--- www/libs/sqlstore.py
+++ www/libs/sqlstore.py
@@ -2,20 +2,30 @@
 import MySQLdb
 
 class Store:
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Store, cls).__new__(
+                                cls, *args, **kwargs)
+        return cls._instance
+
     def __init__(self):
-        self.con = MySQLdb.connect('localhost', 'eye', 'sauron', 'exia')
-        self.c = self.con.cursor()
+        self.con = None
+        self.c = None
 
     def close(self):
         self.con.close()
 
     def execute(self, sql):
+        self.con = MySQLdb.connect('localhost', 'eye', 'sauron', 'exia')
+        self.c = self.con.cursor()
         try:
             ret = self.c.execute(sql)
             self.con.commit()
             return ret
         except:
             import traceback; traceback.print_exc()
+        self.close()
 
     def fetchall(self):
         return self.c.fetchall()
