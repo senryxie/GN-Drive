0b22ca02dea8175009fca9b1d1681e7ec2af8e06
ofshellohicy <ofshellohicy@gmail.com>
2012-03-16 03:08:45 +0800

    fix fabfile for sudo

--- tools/fabfile.py
+++ tools/fabfile.py
@@ -1,18 +1,22 @@
-from fabric.api import sudo, cd, env#, run
+from fabric.api import sudo, cd, env, run
 
 env.hosts=['ofshellohicy@gundam00']
 
 def restart_mysql():
-    sudo('sudo /etc/init.d/mysql restart', user='ofshellohicy')
+    sudo('/etc/init.d/mysql restart', user='root')
 
 def restart_php():
-    sudo('sudo /etc/init.d/php-fastcgi restart', user='ofshellohicy')
+    sudo('/etc/init.d/php-fastcgi restart', user='root')
 
 def restart_nginx():
-    sudo('sudo /etc/init.d/nginx restart', user='ofshellohicy')
+    sudo('/etc/init.d/nginx restart', user='root')
 
 def update_drive():
     with cd('/home/nginx/GN-Drive'):
         sudo('git fetch origin',user='nginx')
         sudo('git rebase origin/master',user='nginx')
-        sudo('sudo /etc/init.d/snap-fastcgi restart',user='ofshellohicy')
+        sudo('/etc/init.d/snap-fastcgi restart',user='root')
+
+def see_letter():
+    with cd('/home/nginx/'):
+        run('cat dead.letter')
