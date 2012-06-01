cffcaea2a1023d1210355cba6ea1a285d28c903b
ofshellohicy <ofshellohicy@gmail.com>
2012-06-01 16:45:14 +0800

    add fabric for GN-drive

---
+++ tools/fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import sudo, cd, env#, run
+
+env.hosts=['gundam00']
+
+def restart_mysql():
+    sudo('sudo /etc/init.d/mysql restart', user='ofshellohicy')
+
+def restart_php():
+    sudo('sudo /etc/init.d/php-fastcgi restart', user='ofshellohicy')
+
+def restart_nginx():
+    sudo('sudo /etc/init.d/nginx restart', user='ofshellohicy')
+
+def update_drive():
+    with cd('/home/nginx/GN-Drive'):
+        sudo('git fetch origin',user='nginx')
+        sudo('git rebase origin/master',user='nginx')
+        sudo('sudo /etc/init.d/snap-fastcgi restart',user='ofshellohicy')
